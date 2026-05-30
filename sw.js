2c06bd23e9b930eb39a8f8e1625229a5eba8cbcd
Leon Fernandes <90561322+IceCreamBacha@users.noreply.github.com>
2026-05-30 15:59:57 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'homecalc-v1';
+const CACHE = 'homecalc-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg',
   'https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js',
